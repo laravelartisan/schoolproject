73d022ff498ba0db551b65cc182d4eaffb5072f7
rajasntbd <raja@sntbd.com>
2015-09-07 10:21:12 +0600

    dashboard data table removed

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,10 +1,6 @@
 @extends('admin.layouts.admin')
 
-@section('head')
 
-  <link rel="stylesheet" href="{{ asset('bower_components/admin-lte/plugins/datatables/dataTables.bootstrap.css') }}">
-
-  @endsection
 
 @section('content')
 
@@ -446,79 +442,7 @@
         </section><!-- /.content -->
 
 
- <div class="row">
-     <div class="col-sm-12">
-         <div class="box">
-             <div class="box-header">
-                 <h3 class="box-title">Data Table With Full Features</h3>
-             </div><!-- /.box-header -->
-             <div class="box-body">
-                 <table id="example1" class="table table-bordered table-striped">
-                     <thead>
-                     <tr>
-                         <th>Rendering engine</th>
-                         <th>Browser</th>
-                         <th>Platform(s)</th>
-                         <th>Engine version</th>
-                         <th>CSS grade</th>
-                     </tr>
-                     </thead>
-                     <tbody>
 
-                     @for ($i = 0; $i < 15; $i++)
-                       <tr>
-                         <td>Trident</td>
-                         <td>Internet
-                           Explorer 4.0</td>
-                         <td>Win 95+</td>
-                         <td> 4</td>
-                         <td>X</td>
-                       </tr>
-                     @endfor
-
-
-
-
-                     <tr>
-                         <td>Other browsers</td>
-                         <td>All others</td>
-                         <td>-</td>
-                         <td>-</td>
-                         <td>U</td>
-                     </tr>
-                     </tbody>
-                     <tfoot>
-                     <tr>
-                         <th>Rendering engine</th>
-                         <th>Browser</th>
-                         <th>Platform(s)</th>
-                         <th>Engine version</th>
-                         <th>CSS grade</th>
-                     </tr>
-                     </tfoot>
-                 </table>
-             </div><!-- /.box-body -->
-         </div><!-- /.box -->
-     </div>
-</div>
 @endsection
 
 
-@section('datascript')
-  <script src="{{ asset('bower_components/admin-lte/plugins/datatables/jquery.dataTables.min.js') }}"></script>
-  <script src="{{ asset('bower_components/admin-lte/plugins/datatables/dataTables.bootstrap.min.js') }}"></script>
-
-  <script>
-    $(function () {
-      $("#example1").DataTable();
-      $('#example2').DataTable({
-        "paging": true,
-        "lengthChange": false,
-        "searching": false,
-        "ordering": true,
-        "info": true,
-        "autoWidth": false
-      });
-    });
-  </script>
-  @endsection
